3823ecdc06eadaea9408745ca526f1434bf281a8
Mads Søndergaard <Santiak@kobuta.me>
2021-05-26 13:58:38 +0200

    [NEW] Bog program

---
+++ Bog/Bog.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bog
+{
+    public class Bog
+    {
+        #region Fields
+        string titel;
+        string author;
+        readonly string Isbn; 
+        List<Bog> books = new List<Bog>();
+        #endregion
+
+        #region Constructor
+        public Bog(string titel, string author, string Isbn)
+        {
+            this.titel = titel;
+            this.author = author;
+            this.Isbn = Isbn;
+        }
+        #endregion
+
+        #region Properties
+        public List<Bog> Books
+        {
+            get
+            {
+                foreach (Bog book in books) ////// HER TIL
+            }
+        }
+
+        public string Titel
+        {
+            get
+            {
+                return titel;
+            }
+            set
+            {
+                titel = value;
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+            set
+            {
+                author = value;
+            }
+        }
+
+        public string ISBN
+        {
+            get
+            {
+                return Isbn;
+            }
+        }
+
+        #endregion
+    }
+
+   
+}
---
+++ Bog/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
