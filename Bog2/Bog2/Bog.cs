85d2e392788e63658f2b688d7da5c80a2679c693
Mads Søndergaard <Santiak@kobuta.me>
2021-05-27 09:22:19 +0200

    [NEW] Created Bog2 as a .NET Framework to allow for class diagrams

---
+++ Bog2/Bog2/Bog.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bog
+{
+    public class Bog
+    {
+        // Lektion 1.2: Omstruktureret til auto-properties.
+        #region Fields
+        public string titel { get; set; }
+        public string author { get; set; }
+        public string Isbn { get; set; } 
+        public List<Bog> books { get; set; } = new List<Bog>();
+        #endregion
+
+        #region Lektion 1.1
+        #region Constructor
+        public Bog(string titel, string author, string Isbn)
+        {
+            this.titel = titel;
+            this.author = author;
+            this.Isbn = Isbn;
+        }
+        #endregion
+
+        #region Properties // Kommenteret ud pga. Lektion 1.2
+       /* public List<Bog> Books
+        {
+            get
+            {
+                return books;
+            }
+        }
+
+        public string Titel
+        {
+            get
+            {
+                return titel;
+            }
+            set
+            {
+                titel = value;
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+            set
+            {
+                author = value;
+            }
+        }
+
+        public string ISBN
+        {
+            get
+            {
+                return Isbn;
+            }
+        }*/
+
+        #endregion 
+
+        #region Methods for fetching data
+        public void GetBook()
+        {
+            Console.WriteLine("Titel:\t\t\tAuthor:\t\t\tISBN:");
+            Console.WriteLine($"{titel}\t\t{author}\t\t{Isbn}");
+        }
+
+        public static void GetBooks(List<Bog> arrBooks)
+        {
+            Console.WriteLine("Titel:\t\t\t\tAuthor:\t\t\tISBN:");
+            foreach (Bog book in arrBooks)
+            {
+                Console.WriteLine($"{book.titel}\t\t\t\t{book.author}\t\t{book.Isbn}");
+            }
+        }
+        #endregion
+        #endregion
+
+        #region Lektion 2.1
+        // Tilføjer en constructor uden argument, da man ellers ikke kan bruge object initializing.
+        public Bog() { }
+
+        public Bog(string titel)
+        {
+            this.titel = titel;
+        }
+        #endregion
+    }
+
+
+
+
+}
---
+++ Bog2/Bog2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Bog> books = new List<Bog>();
+
+            books.Add(new Bog("Jordhavet", "Ursula K LeQuin", "123"));
+            books.Add(new Bog("From Bach To Bacteria And Back", "Daniel Dennett", "234"));
+
+
+            #region Lektion 1.1
+            Console.WriteLine("\n\n######## LEKTION 1.1:");
+            books[0].GetBook();
+            Console.WriteLine("----");
+            Bog.GetBooks(books);
+            #endregion
+
+            #region Lektion 1.2
+            Console.WriteLine("\n\n######## LEKTION 1.2:");
+            Bog book1 = new Bog("Learn Python the Hard Way", "Zed Shaw", "345");
+            book1.GetBook();
+            book1.titel = "Learn C# the Hard Way";
+            book1.GetBook();
+            books[0].author = "Ursula K. LeQuin";
+            books[0].GetBook();
+            #endregion
+
+            #region Lektion 2.1
+            Bog book2 = new Bog { titel = "Zalt", author = "Jon Bing", Isbn = "345" };
+            Console.WriteLine("\n\n\n######## LEKTION 2.1:");
+            book2.GetBook();
+
+            Bog book3 = new Bog("Azur") { author = "Jon Bing", Isbn = "456" };
+            book3.GetBook();
+            #endregion
+
+            #region Lektion 2.x
+            Blad mag1 = new Blad { magName = "Journal of Consciousness Studies", issue = 1, pCount = 129 };
+            mag1.GetMag();
+            #endregion
+        }
+    }
+}
