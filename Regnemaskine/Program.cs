4838ded50c8619a4e222b1537004ec3dc3f778e9
Mads Søndergaard <Santiak@kobuta.me>
2021-06-01 11:24:51 +0200

    [NEW] Regnemaskine

---
+++ Regnemaskine/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+delegate int Udregn(int x, int y);
+namespace Regnemaskine
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int talEt = 5;
+            int talTo = 3;
+
+            Console.WriteLine("Angiv udregningsmetode:");
+            Console.WriteLine("[1] Adder");
+            Console.WriteLine("[2] Subtrakter");
+
+            switch (Console.ReadLine())
+            { 
+                case "1":
+                    {
+                        Udregn addEt = new Udregn(Add);
+
+                        int result = addEt(talEt, talTo);
+                        Console.WriteLine(result);
+                        break;
+                    }
+                case "2":
+                    {
+                        Udregn subEt = new Udregn(Sub);
+                        Console.WriteLine(subEt(talEt, talTo));
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Ugyldigt Valg");
+                    break;
+            }
+            
+
+            
+
+            static int Add(int x, int y)
+            {
+                return x + y;
+            }
+
+            static int Sub(int x, int y)
+            {
+                return x - y;
+            }
+
+        }
+        
+    }
+    
+}
