f9e49a40fbac7ea9fdf6e8977e5657d76f7dcc5b
Mads Søndergaard <Santiak@kobuta.me>
2021-06-01 11:06:25 +0200

    [NEW] Exception Handling

---
+++ Exceptions/DatoCheck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptions
+{
+    class DatoCheck
+    {
+        public static void CheckDato(string user)
+        {
+            DateTime.Parse(user);
+        }
+
+        public static void CheckTruth(string user)
+        {
+            Console.WriteLine(DateTime.Now.AddDays(-1).Date);
+            Console.WriteLine(DateTime.Parse(user).Date);
+            if (DateTime.Now.AddDays(-1).Date == DateTime.Parse(user).Date)
+                Console.WriteLine("Correct");
+            else
+                Console.WriteLine("Incorrect");
+        }
+    }
+}
---
+++ Exceptions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Today;
+            string user;
+
+            Console.WriteLine("The day before today was the:");
+            user = Console.ReadLine();
+
+            try
+            {
+                DatoCheck.CheckDato(user);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Input value invalid. Please use the format [dd-mm-yyyy].");
+            }
+            finally
+            {
+                DatoCheck.CheckTruth(user);
+            }
+        }
+
+    }
+}
