e2003232072f9863dad8a0f0734579242e43ccbb
Mads Søndergaard <Santiak@kobuta.me>
2021-06-01 11:45:10 +0200

    [NEW] Anon Function/Method

---
+++ Anon/DelegatePraktik.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public delegate void MyDelegate(int p, int q);
+
+class Program
+{
+    static void Main()
+    {
+        MyDelegate arithMethod = new MyDelegate(Max);
+        arithMethod(3, 7);
+    }
+
+    static void Max(int c, int d)
+    {
+        if (c > d)
+            Console.WriteLine($"Det største af tallene er: {c}");
+        else
+            Console.WriteLine($"Det største af tallene er: {d}");
+    }
+}
---
+++ DelegatePraktik.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+delegate int MyDelegate(int p, int q);
+
+class Program
+{
+    static void Main()
+    {
+        MyDelegate arithMethod = Max;
+
+        Console.WriteLine("Det største af tallene er: {0}", arithMethod(3, 7));
+    }
+
+    static int Max(int c, int d)
+    {
+        if (c > d)
+            return c;
+        else
+            return d;
+    }
+}
