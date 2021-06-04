a533b5414f4eeee198e4d8439e931f7d8c9e7280
Mads Søndergaard <Santiak@kobuta.me>
2021-06-04 11:08:06 +0200

    [NEW] Added Menu to RentACar (in a new project)

--- RentACar/Program.cs
+++ RentACar/Program.cs
@@ -19,14 +19,14 @@ namespace RentACar
             Console.WriteLine("------- Opretter et RentACar objekt (som er en liste af Car objekter -------");
             myRentACar.UdskrivBil();
 
-            Console.WriteLine("\n\n\n------- Finder og udskriver et bestemt Car objekt i RentACar list-objektet ------");
+            Console.WriteLine("\n\n\n------- Finder og udskriver et bestemt Car objekt (1) i RentACar list-objektet ------");
             myRentACar.UdskrivBil(1);
 
             Console.WriteLine("\n\n\n------- Opretter (og tilføjer) et nyt Car objekt til RentACar listen via en metode ------");
             myRentACar.OpretNyBil("45678", "KIA", "Gul");
             myRentACar.UdskrivBil(3);
 
-            Console.WriteLine("\n\n\n------- Fjerner et af objekterne fra listen, via en metode til at finde indeks nummeret på Car objektet i RentACar listen ------");
+            Console.WriteLine("\n\n\n------- Fjerner et af objekterne (2) fra listen, via en metode til at finde indeks nummeret på Car objektet i RentACar listen ------");
             myRentACar.SletBil(2);
             myRentACar.UdskrivBil();
         }
---
+++ RentACarMenu/RentACar.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar
+{
+    public class RentACar
+    {
+        List<Car> cars = new List<Car>();
+
+        public RentACar()
+        {
+            cars.Add(new Car("12345", "Skoda", "Grøn"));
+            cars.Add(new Car("23456", "Toyota", "Rød"));
+            cars.Add(new Car("34567", "Ford", "Blå"));
+        }
+
+        public void UdskrivBil()
+        {
+            foreach (Car c in cars)
+            {
+                Console.WriteLine(c.HentBil());
+            }
+        }
+
+        public void UdskrivBil(int carId)
+        {
+            Console.WriteLine(cars[FindIndex(carId)].HentBil());
+        }
+
+        private int FindIndex(int carId)
+        {
+            int counter = 0;
+            foreach (Car c in cars)
+            {
+                if (c.id != carId)
+                {
+                    counter++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return counter;
+        }
+
+        public void OpretNyBil(string regNr, string mærke, string farve)
+        {
+            cars.Add(new Car(regNr, mærke, farve));
+        }
+        public void OpretNyBil()
+        {
+            Console.Write("Please provide the registration number of the car: ");
+            string regNr = Console.ReadLine();
+
+            Console.Write("Please specify the brand of car: ");
+            string mærke = Console.ReadLine();
+
+            Console.Write("What color is this magnificent beast!?");
+            string farve = Console.ReadLine();
+
+            OpretNyBil(regNr, mærke, farve);
+        }
+
+        public void SletBil(int id)
+        {
+            cars.RemoveAt(FindIndex(id));
+        }
+
+        public void SletBil()
+        {
+            Console.Write("Please specify the ID of the car you wish to remove from the list: ");
+            cars.RemoveAt(FindIndex(int.Parse(Console.ReadLine()))); 
+            /* This is a bit over-nested, but in broad terms:
+            - RemoveAt needs the index of the item in the list
+            - FindIndex finds the index of an item (i.e. Car), based on that items assigned ID.
+            - int.Parse takes the input from the user, a string, and converts its to an int value, which is the accepted argument type of the FindIndex parameter.
+             */
+        }
+
+    }
+}
